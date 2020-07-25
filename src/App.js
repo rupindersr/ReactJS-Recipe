71db0d83bdc2a045ec58202dc7b9f8260e235a6e
rupindersr <rupindersinghsr@gmail.com>
2020-07-24 22:04:17 -0400

    First push v1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import Recipe from './Recipe';
 import './App.css';
 
 function App() {
+  const APP_ID = "3cbde7b5";
+  const APP_KEY = "b1da7e8e8fab5929936063492b0bcfde";
+
+const[recipes, setRecipes] = useState([]);
+const [search, setSearch] = useState("");
+const[query, setQuery] = useState('chicken');
+
+  useEffect (( ) => {
+    getRecipes();
+  
+    console.log('we are Fetching Data');
+    
+  }, [query])
+  const getRecipes = async () => {
+
+  
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+      );
+    const data = await response.json();
+   
+    setRecipes(data.hits);
+     
+  
+  }
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+    
+  }
+
+  const getSearch = e => {
+   
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="text" value={search} onChange={updateSearch} />
+        <button className="search-button" type="submit" >
+         Search
+        </button>
+      </form>
+      <div className="recipes">
+      {
+      recipes.map(recipe => (
+        <Recipe key={recipe.recipe.label} title={recipe.recipe.label} calories={recipe.recipe.calories}
+        image={recipe.recipe.image}
+        ingredients={recipe.recipe.ingredients}
+        />
+      ))}
+      </div>
     </div>
   );
 }
